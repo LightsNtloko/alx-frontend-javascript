d2a445babf27fbb2f2af65aebc9d8989c16dc261
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-10-02 02:00:21 +0000

    A getFullBudgetObject that uses ES6 method properties in the fullBudget object

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,12 +4,13 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return '${income}';
+    getIncomeInDollars(income) {
+      return `$${income}`;
     },
-    getIncomeInEuros() {
-      return '${income} euros';
+    getIncomeInEuros(income) {
+      return `${income} euros`;
     },
   };
+
   return fullBudget;
 }
