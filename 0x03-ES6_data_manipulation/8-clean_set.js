48dec2bb3aab0fe5529c80fd67c13a2eaaae5707
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-10-09 00:18:36 +0000

    A function named cleanSet that returns a string of all the set values that start with a specific string (startString). It accepts two arguments: a set (Set) and a startString (String). When a value starts with startString you only append the rest of the string. The string contains all the values of the set separated by -

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || typeof startString !== 'string') {
+  if (!startString || typeof startString !== 'string') {
     return '';
   }
 
-  const filteredValues = [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
+  const result = [];
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.substring(startString.length));
+    }
+  }
 
-  return filteredValues.join('-');
+  return result.join('-');
 }
