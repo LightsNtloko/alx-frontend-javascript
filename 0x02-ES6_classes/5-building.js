1ac977836eb67d89f9c2180c66dad25e54b82287
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-10-27 21:14:01 +0000

    A class named Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    // Check if the class is being instantiated directly
-    if (new.target === Building) {
-      throw new Error("Cannot create an instance of an abstract class");
-    }
     this._sqft = sqft;
   }
 
-  // Getter for 'sqft'
+  // Getter for `sqft`
   get sqft() {
     return this._sqft;
   }
 
-  // Method to ensure subclasses implement this
+  // Abstract method that must be implemented by subclasses
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
