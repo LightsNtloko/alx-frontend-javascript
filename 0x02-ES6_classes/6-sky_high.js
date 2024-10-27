c0a583429d9673c493794551d3a5253e5df6cfb6
LightsNtloko <ST10125711@rcconnect.edu.za>
2024-10-27 21:41:31 +0000

    A class named SkyHighBuilding that extends from Building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building.js';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft); // Pass sqft to the parent class constructor
+    this._floors = floors; // Assign floors
+  }
+
+  // Getter for sqft (inherited from Building)
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // Override evacuationWarningMessage method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
