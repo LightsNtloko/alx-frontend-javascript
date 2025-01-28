637b58632a7244f273367371499619cb64d4136d
LightsNtloko <ST10125711@rcconnect.edu.za>
2025-01-28 12:00:21 +0000

    Hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -45,4 +45,3 @@ const student4 = new StudentALX('Donald', 'Bush', class2019);
 const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
 export const listOfStudents = [student1, student2, student3, student4, student5];
-
